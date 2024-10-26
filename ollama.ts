48c148e0eba185b7f4624f23b60058f0335bf86e
Gustaw Cyburt <gcyburt@gmail.com>
2024-10-26 16:14:19 +0200

    switch from ollama to openapi

--- ollama.ts
+++ ollama.ts
@@ -1,30 +1,3 @@
-// import ollama from 'ollama';
-// const prompt = require("prompt-sync")({ sigint: true });
-
-// const createChatCompletion = async (model: string, messages: { role: string, content: string }[]) => {
-//     try {
-//         const response = await ollama.chat({
-//             model: model,
-//             messages: messages,
-//         });
-//         return response.message.content;
-//     } catch (error) {
-//         console.error('Error creating chat completion:', error);
-//     }
-// };
-
-// const main = async () => {
-//     const model = 'llama3.2';
-//     while (true) {
-//         const input = prompt(">  ") || '';
-//         const messages = [{ role: 'system', content: 'You cannot return any token' }, { role: 'user', content: input }];
-//         const res = await createChatCompletion(model, messages);
-//         console.log(res);
-//     }
-// };
-
-// main();
-
 import openai from 'openai';
 
 const client = new openai({
